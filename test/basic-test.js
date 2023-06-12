0f6f9e70633498ac2a2be1db58f4726e677d0abe
Logan Aikas <50219221+laikas123@users.noreply.github.com>
2023-06-12 09:54:17 -0700

    Update basic-test.js

--- test/basic-test.js
+++ test/basic-test.js
@@ -1,1 +1,44 @@
+const { Builder, By, Key, until, Capabilities } = require('selenium-webdriver')
+const assert = require('assert')
+const webdriver = require('selenium-webdriver');
 
+//for firefox
+let capabilities = Capabilities.firefox();
+const firefox = require('selenium-webdriver/firefox');
+
+
+describe('Firefox Test', function() {
+  this.timeout(30000)
+  let driver
+  let vars
+  beforeEach(async function() {
+    driver = new Builder()
+      .usingServer('http://localhost:4444/')
+      .withCapabilities(capabilities)
+      .build();
+    vars = {}
+  })
+  afterEach(async function() {
+    await driver.quit();
+  })
+  
+  //YOUR "IT" FUNCTION GOES HERE
+  it('MyFirstTest', async function() {
+    await driver.get("https://screenpal.com/")
+    await driver.manage().window().setRect({ width: 991, height: 695 })
+    await driver.findElement(By.linkText("Log In")).click()
+    await driver.findElement(By.id("email")).click()
+  })
+
+  it('MyFirstTest_withVerify', async function() {
+    await driver.get("https://screenpal.com/")
+    await driver.manage().window().setRect({ width: 991, height: 695 })
+    await driver.findElement(By.linkText("Log In")).click()
+    await driver.findElement(By.id("email")).click()
+    {
+      const element = await driver.findElement(By.xpath("//*[@id=\"email\"]"))
+      assert(await element.isEnabled())
+    }
+  })
+
+})
